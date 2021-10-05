82fc8c0f609e01b9a67193af31abf966a1e0591a
Matthieu Gras <grasm@student.ethz.ch>
2021-10-05 22:51:09 +0200

    bla

--- src/algorithm_verified.ml
+++ src/algorithm_verified.ml
@@ -17,8 +17,9 @@ let dump_formula dbschema f =
   let sf = convert_formula_serialize dbschema f in
   let json = yojson_of_formula sf in
   let str_json_pretty = Yojson.Safe.pretty_to_string json in
-  let str_json_compact = Yojson.Safe.to_string json in
-  Printf.printf "Pretty:\n%s\n\nCompact:\n%s\n" str_json_pretty str_json_compact;
+  (*let str_json_compact = Yojson.Safe.to_string json in *)
+  Printf.printf "%s\n" str_json_pretty;
+  (*Printf.printf "Pretty:\n%s\n\nCompact:\n%s\n" str_json_pretty str_json_compact;*)
   exit 0
 
 let monitor dbschema logfile fv f =
