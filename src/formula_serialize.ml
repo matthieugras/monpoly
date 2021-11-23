fdd3e82298ff3f21bbe6d2e3243a0e765c3d26b5
Matthieu Gras <grasm@student.ethz.ch>
2021-11-23 17:00:45 +0100

    formatting

--- src/formula_serialize.ml
+++ src/formula_serialize.ml
@@ -137,7 +137,7 @@ let rec formula_to_verified : formula -> Verified.Monitor.formula = function
   | Pred (x1, x2) -> Pred (x1, List.map trm_to_verified x2)
   | Let (x1, x2, x3) -> Let (x1, formula_to_verified x2, formula_to_verified x3)
   | LetPast (x1, x2, x3) ->
-      LetPast (x1, formula_to_verified x2, formula_to_verified x3)
+    LetPast (x1, formula_to_verified x2, formula_to_verified x3)
   | Eq (x1, x2) -> Eq (trm_to_verified x1, trm_to_verified x2)
   | Less (x1, x2) -> Less (trm_to_verified x1, trm_to_verified x2)
   | LessEq (x1, x2) -> LessEq (trm_to_verified x1, trm_to_verified x2)
@@ -147,41 +147,41 @@ let rec formula_to_verified : formula -> Verified.Monitor.formula = function
   | Ands x -> Ands (List.map formula_to_verified x)
   | Exists x -> Exists (formula_to_verified x)
   | Prev ((x1, x2), x3) ->
-      Prev (interval_to_verified x1 x2, formula_to_verified x3)
+    Prev (interval_to_verified x1 x2, formula_to_verified x3)
   | Next ((x1, x2), x3) ->
-      Next (interval_to_verified x1 x2, formula_to_verified x3)
+    Next (interval_to_verified x1 x2, formula_to_verified x3)
   | Since (x1, (x2, x3), x4) ->
-      Since
-        ( formula_to_verified x1,
-          interval_to_verified x2 x3,
-          formula_to_verified x4 )
+    Since
+      ( formula_to_verified x1,
+        interval_to_verified x2 x3,
+        formula_to_verified x4 )
   | Until (x1, (x2, x3), x4) ->
-      Until
-        ( formula_to_verified x1,
-          interval_to_verified x2 x3,
-          formula_to_verified x4 )
+    Until
+      ( formula_to_verified x1,
+        interval_to_verified x2 x3,
+        formula_to_verified x4 )
   | MatchF ((x1, x2), x3) ->
-      MatchF (interval_to_verified x1 x2, regex_to_verified x3)
+    MatchF (interval_to_verified x1 x2, regex_to_verified x3)
   | MatchP ((x1, x2), x3) ->
-      MatchP (interval_to_verified x1 x2, regex_to_verified x3)
+    MatchP (interval_to_verified x1 x2, regex_to_verified x3)
   | Agg (x1, (x2, x3), x4, x5, x6) ->
-      Agg
-        ( nat_to_verified x1,
-          (agg_type_to_verified x2, cst_to_verified x3),
-          nat_to_verified x4,
-          trm_to_verified x5,
-          formula_to_verified x6 )
+    Agg
+      ( nat_to_verified x1,
+        (agg_type_to_verified x2, cst_to_verified x3),
+        nat_to_verified x4,
+        trm_to_verified x5,
+        formula_to_verified x6 )
   | TP x -> TP (trm_to_verified x)
   | TS x -> TS (trm_to_verified x)
   | TPTS (x1, x2) -> And (TP (trm_to_verified x1), TS(trm_to_verified x2))
 
 and regex_to_verified : regex -> Verified.Monitor.formula Verified.Monitor.regex
-    = function
-  | Skip x -> Skip (nat_to_verified x)
-  | Test x -> Test (formula_to_verified x)
-  | Plusa (x1, x2) -> Plusa (regex_to_verified x1, regex_to_verified x2)
-  | Times (x1, x2) -> Times (regex_to_verified x1, regex_to_verified x2)
-  | Star x -> Star (regex_to_verified x)
+  = function
+    | Skip x -> Skip (nat_to_verified x)
+    | Test x -> Test (formula_to_verified x)
+    | Plusa (x1, x2) -> Plusa (regex_to_verified x1, regex_to_verified x2)
+    | Times (x1, x2) -> Times (regex_to_verified x1, regex_to_verified x2)
+    | Star x -> Star (regex_to_verified x)
 
 let convert_cst = function
   | Int x -> EInt (Z.of_int x)
@@ -191,8 +191,8 @@ let convert_cst = function
   | Regexp _ -> unsupported "Regular expression constant are not supported"
 
 let convert_var fvl bvl a = nat_of_int (try (Misc.get_pos a bvl)
-    with Not_found -> (List.length bvl) + (try Misc.get_pos a fvl
-        with Not_found -> List.length fvl))
+                                        with Not_found -> (List.length bvl) + (try Misc.get_pos a fvl
+                                                                               with Not_found -> List.length fvl))
 
 let convert_term fvl bvl =
   let rec convert = function
@@ -211,21 +211,21 @@ let convert_term fvl bvl =
   convert
 
 let convert_interval (l,r) =
-    let lm = match l with
+  let lm = match l with
     | OBnd a -> (a+1)
     | CBnd a -> a
     | Inf -> unsupported ("Unsupported interval " ^ (string_of_interval (l,r)))
-    in
-    let um = match r with
+  in
+  let um = match r with
     | OBnd a -> Some (a-1)
     | CBnd a ->  Some a
     | Inf -> None in
-    match um with
-    | None -> ((nat_of_int lm), Infinity_enat)
-    | Some um ->
-        if lm <= um
-        then ((nat_of_int lm), (enat_of_int um))
-        else unsupported ("Unsupported interval " ^ (string_of_interval (l,r)))
+  match um with
+  | None -> ((nat_of_int lm), Infinity_enat)
+  | Some um ->
+    if lm <= um
+    then ((nat_of_int lm), (enat_of_int um))
+    else unsupported ("Unsupported interval " ^ (string_of_interval (l,r)))
 
 let convert_agg_op = function
   | Cnt -> Agg_Cnt
@@ -248,62 +248,62 @@ let convert_formula_serialize dbschema f =
   let free_vars = MFOTL.free_vars f in
   let truth = Equal (Cst (Int 0), Cst (Int 0)) in
   let rec createExists n f = match n with
-  | 0 -> f
-  | n -> createExists (n-1) (Exists f)
+    | 0 -> f
+    | n -> createExists (n-1) (Exists f)
   in
   let rec convert_formula_vars fvl bvl lets = function
-  | Equal (t1,t2) -> Eq (convert_term fvl bvl t1, convert_term fvl bvl t2)
-  | Less (t1,t2) -> Less (convert_term fvl bvl t1, convert_term fvl bvl t2)
-  | LessEq (t1,t2) -> LessEq (convert_term fvl bvl t1, convert_term fvl bvl t2)
-  | Pred p ->
+    | Equal (t1,t2) -> Eq (convert_term fvl bvl t1, convert_term fvl bvl t2)
+    | Less (t1,t2) -> Less (convert_term fvl bvl t1, convert_term fvl bvl t2)
+    | LessEq (t1,t2) -> LessEq (convert_term fvl bvl t1, convert_term fvl bvl t2)
+    | Pred p ->
       let (n, a, tl) = p in
       if List.mem n special_predicates && not (List.mem (n, a) lets)
       then convert_special_predicate1 fvl bvl p
       else Pred (n, List.map (convert_term fvl bvl) tl)
-  | Let (p,f1,f2) ->
+    | Let (p,f1,f2) ->
       let (n,a,ts) = Predicate.get_info p in
       let fvl1 = List.flatten (List.map Predicate.tvars ts) in
       let lets2 = (n, a) :: lets in
       Let (n, convert_formula_vars fvl1 [] lets f1, convert_formula_vars fvl bvl lets2 f2)
-  | LetPast (p,f1,f2) ->
+    | LetPast (p,f1,f2) ->
       let (n,a,ts) = Predicate.get_info p in
       let fvl1 = List.flatten (List.map Predicate.tvars ts) in
       let lets' = (n, a) :: lets in
       LetPast (n, convert_formula_vars fvl1 [] lets' f1, convert_formula_vars fvl bvl lets' f2)
-  | Neg f -> Neg (convert_formula_vars fvl bvl lets f)
-  | And (f1,f2) -> And (convert_formula_vars fvl bvl lets f1, convert_formula_vars fvl bvl lets f2)
-  | Or (f1,f2) -> Or (convert_formula_vars fvl bvl lets f1, convert_formula_vars fvl bvl lets f2)
-  | Implies (f1,f2) -> convert_formula_vars fvl bvl lets (Or ((Neg f1), f2))
-  | Equiv (f1,f2) -> convert_formula_vars fvl bvl lets (And (Implies (f1,f2),Implies(f2,f2)))
-  | Exists (v,f) -> createExists (List.length v) (convert_formula_vars fvl (v@bvl) lets f)
-  | ForAll (v,f) -> convert_formula_vars fvl bvl lets (Neg (Exists (v,(Neg f))))
-  | Aggreg (t_y, y,op,x,glist,f) ->
+    | Neg f -> Neg (convert_formula_vars fvl bvl lets f)
+    | And (f1,f2) -> And (convert_formula_vars fvl bvl lets f1, convert_formula_vars fvl bvl lets f2)
+    | Or (f1,f2) -> Or (convert_formula_vars fvl bvl lets f1, convert_formula_vars fvl bvl lets f2)
+    | Implies (f1,f2) -> convert_formula_vars fvl bvl lets (Or ((Neg f1), f2))
+    | Equiv (f1,f2) -> convert_formula_vars fvl bvl lets (And (Implies (f1,f2),Implies(f2,f2)))
+    | Exists (v,f) -> createExists (List.length v) (convert_formula_vars fvl (v@bvl) lets f)
+    | ForAll (v,f) -> convert_formula_vars fvl bvl lets (Neg (Exists (v,(Neg f))))
+    | Aggreg (t_y, y,op,x,glist,f) ->
       let t_y = match t_y with TCst a -> a | _ -> failwith "Internal error" in
       let attr = MFOTL.free_vars f in
       let bound = Misc.diff attr glist in
       let bvl_f = bound @ bvl in
       Agg (convert_var fvl bvl y,
-        (convert_agg_op op, convert_cst (aggreg_default_value op t_y)),
-        nat_of_int (List.length bound),
-        convert_term fvl bvl_f (Var x),
-        convert_formula_vars fvl bvl_f lets f)
-  | Prev (intv,f) -> Prev ((convert_interval intv), (convert_formula_vars fvl bvl lets f))
-  | Next (intv,f) -> Next ((convert_interval intv), (convert_formula_vars fvl bvl lets f))
-  | Since (intv,f1,f2) -> Since (convert_formula_vars fvl bvl lets f1,convert_interval intv,convert_formula_vars fvl bvl lets f2)
-  | Until (intv,f1,f2) -> Until (convert_formula_vars fvl bvl lets f1,convert_interval intv,convert_formula_vars fvl bvl lets f2)
-  | Eventually (intv,f) -> convert_formula_vars fvl bvl lets (Until (intv,truth,f))
-  | Once (intv,f) -> convert_formula_vars fvl bvl lets (Since (intv,truth,f))
-  | Always (intv,f) -> convert_formula_vars fvl bvl lets (Neg (Eventually (intv,(Neg f))))
-  | PastAlways (intv,f) -> convert_formula_vars fvl bvl lets (Neg (Once (intv,(Neg f))))
-  | Frex (intv, r) -> MatchF (convert_interval intv, convert_re_vars fvl bvl lets r)
-  | Prex (intv, r) -> MatchP (convert_interval intv, convert_re_vars fvl bvl lets r)
-  | f -> unsupported (string_of_formula "Unsupported formula " f)
-and convert_re_vars fvl bvl lets = function
-  | Wild -> wild
-  | Test f -> Test (convert_formula_vars fvl bvl lets f)
-  | Concat (r1,r2) -> Times (convert_re_vars fvl bvl lets r1, convert_re_vars fvl bvl lets r2)
-  | Plus (r1,r2) -> Plusa (convert_re_vars fvl bvl lets r1, convert_re_vars fvl bvl lets r2)
-  | Star r -> Star (convert_re_vars fvl bvl lets r)
+           (convert_agg_op op, convert_cst (aggreg_default_value op t_y)),
+           nat_of_int (List.length bound),
+           convert_term fvl bvl_f (Var x),
+           convert_formula_vars fvl bvl_f lets f)
+    | Prev (intv,f) -> Prev ((convert_interval intv), (convert_formula_vars fvl bvl lets f))
+    | Next (intv,f) -> Next ((convert_interval intv), (convert_formula_vars fvl bvl lets f))
+    | Since (intv,f1,f2) -> Since (convert_formula_vars fvl bvl lets f1,convert_interval intv,convert_formula_vars fvl bvl lets f2)
+    | Until (intv,f1,f2) -> Until (convert_formula_vars fvl bvl lets f1,convert_interval intv,convert_formula_vars fvl bvl lets f2)
+    | Eventually (intv,f) -> convert_formula_vars fvl bvl lets (Until (intv,truth,f))
+    | Once (intv,f) -> convert_formula_vars fvl bvl lets (Since (intv,truth,f))
+    | Always (intv,f) -> convert_formula_vars fvl bvl lets (Neg (Eventually (intv,(Neg f))))
+    | PastAlways (intv,f) -> convert_formula_vars fvl bvl lets (Neg (Once (intv,(Neg f))))
+    | Frex (intv, r) -> MatchF (convert_interval intv, convert_re_vars fvl bvl lets r)
+    | Prex (intv, r) -> MatchP (convert_interval intv, convert_re_vars fvl bvl lets r)
+    | f -> unsupported (string_of_formula "Unsupported formula " f)
+  and convert_re_vars fvl bvl lets = function
+    | Wild -> wild
+    | Test f -> Test (convert_formula_vars fvl bvl lets f)
+    | Concat (r1,r2) -> Times (convert_re_vars fvl bvl lets r1, convert_re_vars fvl bvl lets r2)
+    | Plus (r1,r2) -> Plusa (convert_re_vars fvl bvl lets r1, convert_re_vars fvl bvl lets r2)
+    | Star r -> Star (convert_re_vars fvl bvl lets r)
   in convert_formula_vars free_vars [] [] f
 
 let serial_to_verified_formula = formula_to_verified
