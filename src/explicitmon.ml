c4eabde569400333186580cca639127ddc24b59f
Matthieu Gras <grasm@student.ethz.ch>
2022-05-24 23:33:07 +0200

    More code

--- src/explicitmon.ml
+++ src/explicitmon.ml
@@ -5,6 +5,7 @@ open String
 open Map
 open Set
 open List
+open Db
 
 let explicit_mon_output = ref false
 let curr_id = ref 0
@@ -15,8 +16,25 @@ module New_id(M: Map.S) = struct
       Some id -> (m, id)
       | None -> (
         incr curr_id;
-        (M.add key !curr_id m, !curr_id)
+        M.add key !curr_id m, !curr_id
       )
+
+  let make_new m key =
+    incr curr_id;
+    let old_id = M.find_opt key m in
+    M.update key (fun _ -> Some !curr_id) m, old_id, !curr_id
+
+  let make_new_mult m keys =
+    let m, old_ids, new_ids = List.fold_left (fun (m, old_ids, new_ids) key ->
+      let m, old_id, new_id = make_new m key in
+      m, old_id :: old_ids, new_id :: new_ids
+    ) (m, [], []) keys in
+    m, List.rev old_ids, List.rev new_ids
+  
+  let update_all m a b =
+    List.fold_left (fun m (a, b) -> 
+      M.update a (fun _ -> b) m
+    ) m (List.combine a b)
 end
 
 module Str_int_pair = struct
@@ -31,6 +49,12 @@ module String_set = Set.Make(String)
 
 type var_id = int
 type pred_id = int
+type translation_ctx = 
+{
+  schema: Db.schema;
+  pmap: pred_id Pred_map.t;
+  vmap: var_id String_map.t;
+}
 
 let rec fv_formula bound_vars = function
   | Equal (a,b)
@@ -62,11 +86,9 @@ and fv_trm bound_vars = function
       else String_set.singleton a
     )
   | Cst _ -> String_set.empty
-(* Unary terms *)
   | UMinus a
   | F2i a
   | I2f a -> fv_trm bound_vars a
-(* Binary terms *)
   | Mult (a,b)
   | Div (a,b)
   | Mod (a,b)
@@ -81,31 +103,50 @@ type cst_type =
   | CstLessEq
 
 type simple_op = 
-  | AndAssign of term
-  | AndRel of cst_type * term * term
-  | Exists of var_id list
+  | MAndAssign of term
+  | MAndRel of cst_type * term * term
+  | MExists of var_id list
 
 type predarg =
   | Var of tcst * var_id
   | Cst of cst
 
+type bound = Bnd of tsdiff | Inf
+type interval = bound * bound
+
+let translate_bnd = function
+  | OBnd a -> Bnd (MFOTL.ts_minus a Z.one)
+  | CBnd a -> Bnd a
+  | MFOTL.Inf -> Inf
+
+let translate_intv (a, b) = translate_bnd a, translate_bnd b
+
+let is_inf_intv = function
+  | _, Inf -> true
+  | _ -> false
+
+let if_unbounded = function
+  | Bnd a, Inf -> Some a
+  | Bnd a, Bnd b -> None
+  | _ -> failwith "malformed"
+
 type exformula =
   | MPredicate of pred_id * (predarg list)
   | MTp of predarg
   | MTs of predarg
-  | MTpTs of predarg * predarg
+  | MTpts of predarg * predarg
   | MAnd of exformula * exformula
   | MOr of exformula * exformula
   | MNeg of exformula
   | MPrev of interval * exformula
   | MNext of interval * exformula
   | MOnceB of interval * exformula
-  | MOnceU of tsdiff
+  | MOnceU of tsdiff * exformula
   | MSinceB of interval * exformula * exformula
   | MSinceU of tsdiff * exformula * exformula
-  | FusedSimpleOp of simple_op list
-  | MUntil of exformula * exformula
-  | MEventually of exformula
+  | MFusedSimpleOp of simple_op list * exformula
+  | MUntil of interval * exformula * exformula
+  | MEventually of interval * exformula
 
 
 let create_new_id =
@@ -129,31 +170,87 @@ let translate_pred_args schema_entry vmap terms =
   ) (vmap, [], 0) terms in
   vmap, List.rev ret
 
-let translate_pred name arity terms schema pmap vmap =
+let translate_pred ctx name arity terms =
   let module P = New_id(Pred_map) in
-  let pmap, new_id = P.maybe_make_new pmap (name, arity) in
-  let vmap, trsfd = translate_pred_args (List.assoc name schema) vmap terms in
+  let pmap, new_id = P.maybe_make_new ctx.pmap (name, arity) in
+  let vmap, trsfd = translate_pred_args (List.assoc name ctx.schema) ctx.vmap terms in
   let generic_pred =
     if name == "tp" && arity == 1 then
       MTp (nth trsfd 0)
     else if name = "ts" && arity == 1 then
       MTs (nth trsfd 0)
     else if name = "tpts" && arity == 2 then
-      MTpTs (nth trsfd 0, nth trsfd 1)
+      MTpts (nth trsfd 0, nth trsfd 1)
     else
       MPredicate (new_id, trsfd)
   in
-  generic_pred, pmap, vmap
+  generic_pred, {ctx with pmap; vmap}
 
-let rec translate_formula schema pmap vmap = function
+let rec translate_formula ctx = function
   (* rewriting cases *)
-  | ForAll (v,f) -> translate_formula schema pmap vmap (Neg (Exists (v, (Neg f))))
-  | Equiv (a,b) -> translate_formula schema pmap vmap (And ((Implies (a,b)), (Implies (a,b))))
-  | Implies (a,b) -> translate_formula schema pmap vmap (Or ((Neg a), b))
-  | Always (intv, a) -> translate_formula schema pmap vmap (Neg (Eventually (intv, Neg a)))
-  | Pred (name, arity, terms) -> translate_pred name arity terms schema pmap vmap
-  | _ -> failwith "rofl"
+  | ForAll (v,f) -> translate_formula ctx (Neg (Exists (v, (Neg f))))
+  | Equiv (a,b) -> translate_formula ctx (And ((Implies (a,b)), (Implies (a,b))))
+  | Implies (a,b) -> translate_formula ctx (Or ((Neg a), b))
+  | Always (intv, a) -> translate_formula ctx (Neg (Eventually (intv, Neg a)))
+  | PastAlways (intv, a) -> translate_formula ctx (Neg (Once (intv, Neg a)))
+  (* other cases *)
+  | Pred (name, arity, terms) -> translate_pred ctx name arity terms
+  | Prev (intv, f) ->
+    let f, ctx = translate_formula ctx f in
+    let intv = translate_intv intv in
+    MPrev (intv, f), ctx
+  | Next (intv, f) ->
+    let f, ctx = translate_formula ctx f in
+    let intv = translate_intv intv in
+    MNext (intv, f), ctx
+  | Once (intv, f) ->
+    let f, ctx = translate_formula ctx f in
+    let intv = translate_intv intv in
+    let f = (match if_unbounded intv with
+              | Some k -> MOnceU (k, f)
+              | None -> MOnceB (intv, f)) in
+    f, ctx
+  | Eventually (intv, f) ->
+    let f, ctx = translate_formula ctx f in
+    let intv = translate_intv intv in
+    MEventually (intv, f), ctx
+  | Since (intv, f1, f2) ->
+      let f1, ctx = translate_formula ctx f1 in
+      let f2, ctx = translate_formula ctx f2 in
+      let intv = translate_intv intv in
+      let f = (match if_unbounded intv with
+                | Some k -> MSinceU (k, f1, f2)
+                | None -> MSinceB (intv, f1, f2)) in
+      f, ctx
+  | Until (intv, f1, f2) ->
+      let f1, ctx = translate_formula ctx f1 in
+      let f2, ctx = translate_formula ctx f2 in
+      MUntil (translate_intv intv, f1, f2), ctx
+  | Or (f1, f2) ->
+      let f1, ctx = translate_formula ctx f1 in
+      let f2, ctx = translate_formula ctx f2 in
+      MOr (f1, f2), ctx
+  | Exists (_, _) as f ->
+      transform_fused_op ctx [] f
+  | And (_, _) as f ->
+      (* possibly a fused op *)
+      transform_fused_op ctx [] f
+  | _ -> failwith "unsupported fragment"
+and transform_fused_op ctx sops = function
+  | Exists (vars, f) ->
+      let module S = New_id(String_map) in
+      let vmap, old_ids, new_ids = S.make_new_mult ctx.vmap vars in
+      let f, ctx = transform_fused_op {ctx with vmap} ((MExists new_ids) :: sops) f in
+      let vmap = S.update_all vmap vars old_ids in
+      f, {ctx with vmap}
+  | And (f1, f2) ->
+      failwith "rofl"
+  | f -> 
+      let f, ctx = translate_formula ctx f in
+      MFusedSimpleOp (List.rev sops, f), ctx
+
 
 let make_exformula schema f = 
-  let (f, pmap, _) = translate_formula schema (Pred_map.empty) (String_map.empty) f in
+  let init_ctx = {schema = schema; pmap = Pred_map.empty; vmap = String_map.empty} in
+  let (_, _) = translate_formula init_ctx f in
   "rofl"
